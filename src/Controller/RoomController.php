dab3b533e97eec9464f9d8aefef8340707986f1a
Lunel Clamy <lunelclamy@hotmail.fr>
2022-10-06 18:57:04 +0200

    commit liste d'annonces OK

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RoomRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class RoomController extends AbstractController
 {
     #[Route('/rooms', name: 'app_rooms_index')]
-    public function index(): Response
+    public function index(RoomRepository $roomRepository): Response
     {
+        $rooms = $roomRepository->findAll();
         return $this->render('room/index.html.twig', [
             'controller_name' => 'RoomController',
+            'rooms' => $rooms
         ]);
+    }
+
+    /**
+     * Permet d'afficher une seule Suite
+     * @return Response
+     */
+    #[Route('/rooms/{title}')]
+    public function show($title, RoomRepository $roomRepository) {
+        // Je récupère la Suite qui correspond au title-
+        $room = $roomRepository->findOneByTitle($title);
+
+        return $this->render('room/show.html.twig', [
+            'room' => $room
+        ]);
+
     }
 }
