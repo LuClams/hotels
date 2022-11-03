0cca4b61319498e8edcfc0cbdbbe87ccae3de889
LuClams <lunelclamy@hotmail.fr>
2022-11-03 11:55:13 +0100

    Commit prod OK

--- Script SQL/export_supervisor.sql
+++ Script SQL/export_supervisor.sql
@@ -4,10 +4,10 @@ LIMIT 0, 1000
 
 -- Date: 2022-11-03 10:13
 */
-INSERT INTO `` (`id`,`user_id`,`hostel_id`) VALUES (1,3,1);
-INSERT INTO `` (`id`,`user_id`,`hostel_id`) VALUES (2,2,2);
-INSERT INTO `` (`id`,`user_id`,`hostel_id`) VALUES (4,4,3);
-INSERT INTO `` (`id`,`user_id`,`hostel_id`) VALUES (5,5,4);
-INSERT INTO `` (`id`,`user_id`,`hostel_id`) VALUES (6,6,5);
-INSERT INTO `` (`id`,`user_id`,`hostel_id`) VALUES (7,7,6);
-INSERT INTO `` (`id`,`user_id`,`hostel_id`) VALUES (8,8,7);
+INSERT INTO `supervisor` (`id`,`user_id`,`hostel_id`) VALUES (1,3,1);
+INSERT INTO `supervisor` (`id`,`user_id`,`hostel_id`) VALUES (2,2,2);
+INSERT INTO `supervisor` (`id`,`user_id`,`hostel_id`) VALUES (4,4,3);
+INSERT INTO `supervisor` (`id`,`user_id`,`hostel_id`) VALUES (5,5,4);
+INSERT INTO `supervisor` (`id`,`user_id`,`hostel_id`) VALUES (6,6,5);
+INSERT INTO `supervisor` (`id`,`user_id`,`hostel_id`) VALUES (7,7,6);
+INSERT INTO `supervisor` (`id`,`user_id`,`hostel_id`) VALUES (8,8,7);
--- Script SQL/export_user.sql
+++ Script SQL/export_user.sql
@@ -4,16 +4,16 @@ LIMIT 0, 1000
 
 -- Date: 2022-11-03 10:13
 */
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (1,'carl@example.com','[]','$2y$13$TVapYVN3euWajIsg.Fguredmgyi4mHT.guYwQmObxrMgKbA1C7Bw6','Carl','Clams',0);
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (2,'Kassie@hostel.com','[\"ROLE_SUPERVISOR_HYATT\"]','$2y$13$zg1Su9BJ8dH5bTjoQsbVbu9BN68Dv33mK6GrP2kL2BcEMfpjHyUCS','Kassie','Sterre',0);
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (3,'cgianelli@hostel.com','[\"ROLE_SUPERVISOR_WESTEEN\"]','$2y$13$AcA2ZPSrdfXTkzl78UE3x.9YJfA8rmM2ZW9fZt4yvFaPvhRzoZvaa','Cynthie','Gianelli',0);
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (4,'cyril@hostel.com','[\"ROLE_SUPERVISOR_CAP\"]','$2y$13$Y7/HXi8D4PCpo6mqVvOHleHvRUTMujZ27TGSYlW6D8ewq6QLX.GHC','Cyril','Boli',0);
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (5,'anthony@hostel.com','[\"ROLE_SUPERVISOR_IBIS\"]','$2y$13$ngBGPUY81wdVaJfdklJu6OB5MRSxXyr3UlfQdFsL7l0ykBqKjT/d.','Anthony','Posnett',0);
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (6,'rafael@hostel.com','[\"ROLE_SUPERVISOR_HOTELV\"]','$2y$13$0YHZcd6qkjJfSUdMsAnYCedsOI5p7z.XyBR49c2cVeYnt/QfZQwfO','Rafael','Conklin',0);
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (7,'owen@hostel.com','[\"ROLE_SUPERVISOR_JAZZY\"]','$2y$13$p.953gb1rKVuZb95zJUNNOp/D3JyCwTwViLqNzxTCBFMgARyzrxvO','Owen','Cranmore',0);
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (8,'giovanni@hostel.com','[\"ROLE_SUPERVISOR_YADEL\"]','$2y$13$eH2YoiCCLj384b2TDha1ReMGTAE4Gogb/DVWJM783lvBBgDYhoyfK','Giovanni','Pladen',0);
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (9,'lunelclamytj@outlook.fr','[\"ROLE_ADMIN\"]','$2y$13$yWmFZM44SNH.d7fPxxo3T.JBQ48aJ7mXnzYMtPcoX1pT/6jyBahli','Lunel','Clamy',0);
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (10,'lucy@example.com','[]','$2y$13$LYTnNIucy2DTDnIAfUNndewdd3O6XHz1Kq9lF4YC4p/1K0tp1fFrq','Lucy','Odenisk',0);
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (11,'gilbert@example.com','[]','$2y$13$3sRTwccQT14lF9l0BgpOo.7gUJpqi9I87xi.8I8uneeWHVg/tjJ5u','Gilbert','Clams',0);
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (12,'cecile@example.com','[]','$2y$13$75ErBJ1.uZ1Dzn49awZtRuONI.AlK5L6VD4N/AWt5bW081mriOm3u','Cecile','Calais',0);
-INSERT INTO `` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (13,'serge@example.com','[]','$2y$13$FIq.RbCl8hH./LYAOZ9JLemxBMKjyDedJqkBdyc6WA1KDlKIyCQnO','Serge','Atoma',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (1,'carl@example.com','[]','$2y$13$TVapYVN3euWajIsg.Fguredmgyi4mHT.guYwQmObxrMgKbA1C7Bw6','Carl','Clams',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (2,'Kassie@hostel.com','[\"ROLE_SUPERVISOR_HYATT\"]','$2y$13$zg1Su9BJ8dH5bTjoQsbVbu9BN68Dv33mK6GrP2kL2BcEMfpjHyUCS','Kassie','Sterre',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (3,'cgianelli@hostel.com','[\"ROLE_SUPERVISOR_WESTEEN\"]','$2y$13$AcA2ZPSrdfXTkzl78UE3x.9YJfA8rmM2ZW9fZt4yvFaPvhRzoZvaa','Cynthie','Gianelli',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (4,'cyril@hostel.com','[\"ROLE_SUPERVISOR_CAP\"]','$2y$13$Y7/HXi8D4PCpo6mqVvOHleHvRUTMujZ27TGSYlW6D8ewq6QLX.GHC','Cyril','Boli',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (5,'anthony@hostel.com','[\"ROLE_SUPERVISOR_IBIS\"]','$2y$13$ngBGPUY81wdVaJfdklJu6OB5MRSxXyr3UlfQdFsL7l0ykBqKjT/d.','Anthony','Posnett',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (6,'rafael@hostel.com','[\"ROLE_SUPERVISOR_HOTELV\"]','$2y$13$0YHZcd6qkjJfSUdMsAnYCedsOI5p7z.XyBR49c2cVeYnt/QfZQwfO','Rafael','Conklin',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (7,'owen@hostel.com','[\"ROLE_SUPERVISOR_JAZZY\"]','$2y$13$p.953gb1rKVuZb95zJUNNOp/D3JyCwTwViLqNzxTCBFMgARyzrxvO','Owen','Cranmore',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (8,'giovanni@hostel.com','[\"ROLE_SUPERVISOR_YADEL\"]','$2y$13$eH2YoiCCLj384b2TDha1ReMGTAE4Gogb/DVWJM783lvBBgDYhoyfK','Giovanni','Pladen',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (9,'lunelclamytj@outlook.fr','[\"ROLE_ADMIN\"]','$2y$13$yWmFZM44SNH.d7fPxxo3T.JBQ48aJ7mXnzYMtPcoX1pT/6jyBahli','Lunel','Clamy',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (10,'lucy@example.com','[]','$2y$13$LYTnNIucy2DTDnIAfUNndewdd3O6XHz1Kq9lF4YC4p/1K0tp1fFrq','Lucy','Odenisk',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (11,'gilbert@example.com','[]','$2y$13$3sRTwccQT14lF9l0BgpOo.7gUJpqi9I87xi.8I8uneeWHVg/tjJ5u','Gilbert','Clams',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (12,'cecile@example.com','[]','$2y$13$75ErBJ1.uZ1Dzn49awZtRuONI.AlK5L6VD4N/AWt5bW081mriOm3u','Cecile','Calais',0);
+INSERT INTO `user` (`id`,`email`,`roles`,`password`,`firstname`,`lastname`,`is_verified`) VALUES (13,'serge@example.com','[]','$2y$13$FIq.RbCl8hH./LYAOZ9JLemxBMKjyDedJqkBdyc6WA1KDlKIyCQnO','Serge','Atoma',0);
